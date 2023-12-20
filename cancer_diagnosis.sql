a77df9b2d8c9a494f476e0ab02df030393dded5d
ArtyomKuzmin <aramas.artt@yandex.ru>
2023-12-21 02:11:14 +0300

    fix

--- cancer_diagnosis.sql
+++ cancer_diagnosis.sql
@@ -5,12 +5,13 @@ USE cancer_diagnosis;
 CREATE TABLE CancerType (
     typeid INT NOT NULL AUTO_INCREMENT,
     type VARCHAR(100),
-    diagnos CHAR(1),
+    diagnos VARCHAR(1),
     PRIMARY KEY (typeid)
 );
 
 CREATE TABLE CellNucleus (
     id INT NOT NULL AUTO_INCREMENT,
+    typeid INT,
     radius FLOAT,
     texture FLOAT,
     perimeter FLOAT,
@@ -21,16 +22,12 @@ CREATE TABLE CellNucleus (
     concave_points FLOAT,
     symmetry FLOAT,
     fractal_dimension FLOAT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (typeid) REFERENCES CancerType(typeid) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO CancerType (type, diagnos) VALUES ('SomeType', 'M');
 
-INSERT INTO CellNucleus (radius, texture, perimeter, area, smoothness, 
-compactness, concavity, concave_points, symmetry, fractal_dimension) 
-VALUES (17.99, 10.38, 122.8, 1001, 0.1184, 0.2776, 0.3001, 0.1471, 0.2419, 0.07871);
-
-ALTER TABLE CellNucleus
-ADD CONSTRAINT fk_typeid
-    FOREIGN KEY (typeid)
-    REFERENCES CancerType(typeid);
+INSERT INTO CellNucleus (typeid, radius, texture, perimeter, area, smoothness, compactness, 
+concavity, concave_points, symmetry, fractal_dimension) VALUES (1, 17.99, 10.38, 122.8, 
+1001, 0.1184, 0.2776, 0.3001, 0.1471, 0.2419, 0.07871);
