ec46b34467ac0d4a1950d7ad1feeb50333022dbd
ArtyomKuzmin <aramas.artt@yandex.ru>
2023-12-22 01:02:37 +0400

    fix

--- cancer_diagnosis.sql
+++ cancer_diagnosis.sql
@@ -31,3 +31,9 @@ INSERT INTO CancerType (type, diagnos) VALUES ('SomeType', 'M');
 INSERT INTO CellNucleus (typeid, radius, texture, perimeter, area, smoothness, compactness, 
 concavity, concave_points, symmetry, fractal_dimension) VALUES (1, 17.99, 10.38, 122.8, 
 1001, 0.1184, 0.2776, 0.3001, 0.1471, 0.2419, 0.07871);
+
+LOAD DATA INFILE 'путь к data.csv'
+INTO TABLE CellNucleus
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
